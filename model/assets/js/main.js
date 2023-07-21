8cb57cff347da3461b2bda9a2f3685af59247861
leandro.leite <leandro.leite@lavid.ufpb.br>
2023-07-21 00:23:25 -0300

    alter in comparasion with IMC

--- model/assets/js/main.js
+++ model/assets/js/main.js
@@ -7,7 +7,7 @@ function calcularIMC (evento){
     let imc = peso / (altura * altura);
     console.log(imc.toFixed(2))
 
-    estadoNutricional(imc.toFixed(2))
+    estadoNutricional(imc.toFixed(1))
 }
 
 function limparForms(){
@@ -20,7 +20,7 @@ function estadoNutricional(imc){
      resultado.innerHTML = `Seu IMC é ${imc}: Você está abaixo do peso.`
      console.log('passou1')
    }
-   else if(imc >= 18.5 && imc <= 24.9 ){
+   else if(imc >= 18.5 && imc <= 24.9){
     console.log('passou2')
      resultado.innerHTML = `Seu IMC é ${imc}:  Peso normal.`
    }
@@ -28,10 +28,10 @@ function estadoNutricional(imc){
     console.log('passou3')
      resultado.innerHTML = `Seu IMC é ${imc}:  Sobrepeso.`
    }
-   else if(imc >= 30 && imc <= 34.90 ){
+   else if(imc >= 30 && imc <= 34.9 ){
      resultado.innerHTML = `Seu IMC é ${imc}:  Obesidade grau 1.`
    }
-   else if(imc >= 35 && imc <= 39.90){
+   else if(imc >= 35 && imc <= 39.9){
      resultado.innerHTML = `Seu IMC é ${imc}:  Obesidade grau 2.`
    }
    else if(imc >= 40 ){
