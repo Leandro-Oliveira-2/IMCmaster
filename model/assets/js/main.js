ae63e7cf396bfab77128a2d1990371954d695d95
leandro.leite <leandro.leite@lavid.ufpb.br>
2023-07-21 20:21:28 -0300

    implements a new functio

--- model/assets/js/main.js
+++ model/assets/js/main.js
@@ -1,42 +1,49 @@
 function calcularIMC (evento){
-    evento.preventDefault();
+  evento.preventDefault();
 
-    var peso = document.querySelector('.peso').value;
-    var altura = document.querySelector('.altura').value;
+  var peso = document.querySelector('.peso').value;
+  var altura = document.querySelector('.altura').value;
 
-    let imc = peso / (altura * altura);
-    console.log(imc.toFixed(2))
+  let imc = peso / (altura * altura);
+  console.log(imc.toFixed(2))
 
-    estadoNutricional(imc.toFixed(1))
+  estadoNutricional(imc.toFixed(1))
 }
 
 function limparForms(){
-    form.document.querySelector('.form').reset();
+  form.document.querySelector('.form').reset();
 }
 
 function estadoNutricional(imc){
-    resultado.innerHTML = `Resultado:`
-   if(imc <= 18.5 ){
-     resultado.innerHTML = `Seu IMC é ${imc}: Você está abaixo do peso.`
-     console.log('passou1')
-   }
-   else if(imc >= 18.5 && imc <= 24.9){
-    console.log('passou2')
-     resultado.innerHTML = `Seu IMC é ${imc}:  Peso normal.`
-   }
-   else if(imc >= 25 && imc <= 29.9){
-    console.log('passou3')
-     resultado.innerHTML = `Seu IMC é ${imc}:  Sobrepeso.`
-   }
-   else if(imc >= 30 && imc <= 34.9 ){
-     resultado.innerHTML = `Seu IMC é ${imc}:  Obesidade grau 1.`
-   }
-   else if(imc >= 35 && imc <= 39.9){
-     resultado.innerHTML = `Seu IMC é ${imc}:  Obesidade grau 2.`
-   }
-   else if(imc >= 40 ){
-     resultado.innerHTML = `Seu IMC é ${imc}:  Obesidade grau 3.`
-   }
+
+const nivel = [' Você está abaixo do peso.', ' Peso normal.', ' Sobrepeso.', ' Obesidade grau 1.', 
+' Obesidade grau 2.', '(Obesidade grau 3.' ]
+
+resultado.innerHTML = `Resultado:`
+if(imc <= 18.5 ){
+  resultado.style.backgroundColor = 'aquamarine;'
+  resultado.innerHTML = `Seu IMC é ${imc}: ${nivel[0]}`
+}
+else if(imc >= 18.5 && imc <= 24.9){
+  resultado.style.backgroundColor = 'aquamarine'
+  resultado.innerHTML = `Seu IMC é ${imc}:  ${nivel[1]}`
+}
+else if(imc >= 25 && imc <= 29.9){
+resultado.style.backgroundColor = 'aquamarine'
+resultado.innerHTML = `Seu IMC é ${imc}:  ${nivel[2]}`
+}
+else if(imc >= 30 && imc <= 34.9 ){
+resultado.style.backgroundColor = 'aquamarine'
+resultado.innerHTML = `Seu IMC é ${imc}:  ${nivel[3]}`
+}
+else if(imc >= 35 && imc <= 39.9){
+resultado.style.backgroundColor = 'aquamarine'
+resultado.innerHTML = `Seu IMC é ${imc}:  ${nivel[4]}`
+}
+else if(imc >= 40 ){
+resultado.style.backgroundColor = 'aquamarine'
+resultado.innerHTML = `Seu IMC é ${imc}:  ${nivel[5]}`
+}
 }
 
 const form = document.querySelector('.form');
